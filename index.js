87ffecb5fa7791e4d7d09f1a87e7ad88f9ba4eac
tawsifhye <tawsif.lucse@gmail.com>
2021-11-10 22:08:26 +0600

    API for user added

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Running  Server");
+    res.send("Running  Server of Real Estate");
 })
 
 app.listen(port, () => {
